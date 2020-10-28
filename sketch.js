744ff884ee314d78a88aa2e52137d36aff6c88c5
Johannes-Lionel <antojovita@gmail.com>
2020-10-28 17:51:40 +0530

    platform remove

--- sketch.js
+++ sketch.js
@@ -4,10 +4,11 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
+
 }
 
 function setup(){
@@ -17,7 +18,9 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    //working directry 
+    //staging area
+    //your repositery
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
